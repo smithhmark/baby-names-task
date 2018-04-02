5b40fb836eac97bbaaa4e7719a6c3a2bab036b76
Mark Smith <smith.h.mark@gmail.com>
2018-04-02 14:31:49 -0700

    growing graph functionality

--- namegraph/namegraph.go
+++ namegraph/namegraph.go
@@ -15,5 +15,30 @@ func (g *NameGraph) Size() int {
 }
 
 func (g *NameGraph) AddName(nm string) {
-	g.nodes[nm] = 0
+	_, ok := g.nodes[nm]
+	if !ok {
+		g.nodes[nm] = 0
+	}
+}
+
+func (g *NameGraph) addAdjacency(s1, s2 string) {
+	nodes, ok := g.adjacencies[s1]
+	if !ok {
+		nodes = make([]string, 0, 2)
+	} else {
+		if len(nodes) == cap(nodes) {
+			newNodes := make([]string, len(nodes), 2*cap(nodes))
+			copy(newNodes, nodes)
+			nodes = newNodes
+		}
+	}
+	g.adjacencies[s1] = append(nodes, s2)
+}
+
+
+func (g *NameGraph) AddAdjacency(s1, s2 string) {
+	g.AddName(s1)
+	g.AddName(s2)
+	g.addAdjacency(s1, s2)
+	g.addAdjacency(s2, s1)
 }
--- namegraph/namegraph_test.go
+++ namegraph/namegraph_test.go
@@ -9,14 +9,20 @@ func TestConstruction(t *testing.T) {
 	if g1.Size() != 0 {
 		t.Fatalf("initial size should be Zero")
 	}
+	if len(g1.adjacencies) != 0 {
+		t.Fatalf("initial adjacency list should be empty")
+	}
 	g2 := NewNameGraph()
 	if g2.Size() != 0 {
 		t.Fatalf("initial size should be Zero")
 	}
+	if len(g2.adjacencies) != 0 {
+		t.Fatalf("initial adjacency list should be empty")
+	}
 
 }
 
-func TestAdd(t *testing.T) {
+func TestAddName(t *testing.T) {
 	g := NewNameGraph()
 
 	if g.Size() != 0 {
@@ -26,5 +32,26 @@ func TestAdd(t *testing.T) {
 	if g.Size() != 1 {
 		t.Fatalf("size should be One after an add")
 	}
+	g.AddName("Sue")
+	if g.Size() != 1 {
+		t.Fatalf("size should STILL be One after a repeated add")
+	}
+}
 
+func TestAddAdjacency(t *testing.T) {
+	g := NewNameGraph()
+
+	g.AddAdjacency("Sue", "Susan")
+	if len(g.adjacencies) != 2 {
+		t.Fatalf("Failed to add both names to the adjacency list")
+	}
+	if len(g.nodes) != 2 {
+		t.Fatalf("Failed to add both names to the node list")
+	}
+	g.AddAdjacency("Chris", "Kris")
+	g.AddAdjacency("Chris", "Kristopher")
+	g.AddAdjacency("Chris", "Cristopher")
+	if len(g.adjacencies["Chris"]) != 3 {
+		t.Fatalf("Failed to grow adjacency list")
+	}
 }
